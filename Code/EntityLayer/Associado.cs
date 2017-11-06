80c13353d1562f02da2786a1faf5219fa0e37d18
mgcarvalho <carvalho.mg@gmail.com>
2017-11-06 11:42:27 +0000

    Entities Associado, endereço e Pessoa

--- Code/EntityLayer/Associado.cs
+++ Code/EntityLayer/Associado.cs
@@ -8,7 +8,20 @@ namespace EntityLayer
 {
     public class Associado
     {
-        public virtual string Nome { get; set; }
-        public virtual string Observacao { get; set; }
+        public virtual int AssociadoId { get; set; }
+        public virtual Pessoa Pessoa { get; set; }
+        public virtual ATC ATC { get; set; }
+        public virtual TipoPublico TipoPublicoId { get; set; }
+        public virtual string Cpf { get; set; }
+        public virtual string Rg { get; set; }
+        public virtual string NrMatricula { get; set; }
+        public virtual string Crp { get; set; }
+        public virtual string Crm { get; set; }
+        public virtual string NomeInsFormacao { get; set; }
+        public virtual bool Certificado { get; set; }
+        public virtual DateTime DtCertificacao { get; set; }
+        public virtual bool DivulgarContato { get; set; }
+        public virtual string TipoFormacao { get; set; }
+
     }
 }
